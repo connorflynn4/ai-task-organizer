dd039ae0b8224e25fb5a7ee4961a9939ba330ec4
Connor Flynn <connorflynn4@gmail.com>
2023-06-09 18:49:09 -0700

    drag/drop functionality working

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
-    <Header/> 	
+    <Header/>
+    
+    <Board />
 
-      {/* Board */}
-      <h1> AI Task Organizer </h1>
     </main>
   )
 }
